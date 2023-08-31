a63f62cc54999fd738fa07fa319241f29aa9950e
jingyeong0604 <wlsrud677@naver.com>
2023-08-31 14:41:27 +0900

    feat : 경비 등록을 위한 요청 dto 클래스 생성

---
+++ src/main/java/trippers/triprecorder/dto/ExpenseRegisterDTO.java
@@ -0,0 +1,13 @@
+package trippers.triprecorder.dto;
+
+import java.sql.Timestamp;
+
+public class ExpenseRegisterDTO {
+    private String expenseName;
+    private String place;
+    private String address;
+    private String money;
+    private Timestamp time;
+    private String paymentMethod;
+    private String category;
+}
